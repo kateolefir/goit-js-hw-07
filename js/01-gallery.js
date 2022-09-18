5d1b995a91b2524a65cda44b17e140ee61a091bc
kateolefir <kateryna.olefir@gmail.com>
2022-09-19 00:30:05 +0300

    add 7.1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup)
+galleryContainer.addEventListener('click', onPictureModalClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+    }).join("");
+};
+
+function onPictureModalClick(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const galleryCard = basicLightbox.create(
+        `<img src="${e.target.dataset.source}" width="800" height="600">`);
+    galleryCard.show();
+
+    if (galleryCard.visible()) {
+        galleryContainer.addEventListener('keydown', (e) => {
+            if (e.code === 'Escape') {
+                galleryCard.close()
+            }
+        }
+        );
+    };
+}
+
